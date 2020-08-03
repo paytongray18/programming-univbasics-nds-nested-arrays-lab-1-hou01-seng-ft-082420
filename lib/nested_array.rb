52d94df01ba9e10fc4d6330d5ab3b4003c7ec9de
Payton Gray <paytongray18@gmail.com>
2020-08-03 22:23:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,7 +26,7 @@ end
 
 def matrix_lookup(matrix, row, column)
    produce_storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
-  produce_storage_room[column][row]
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
